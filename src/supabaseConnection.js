f81f1fa676856a13860ba3a2cbfe034b0b11534f
şerife canevi <93770226+SerifeCanevi@users.noreply.github.com>
2024-02-11 18:05:22 +0300

    fix

--- src/supabaseConnection.js
+++ src/supabaseConnection.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
-  "https://pkvnnraectowfazzjneo.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrdm5ucmFlY3Rvd2ZhenpqbmVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2NjI3NDgsImV4cCI6MjAyMzIzODc0OH0.8slt4OM2EJ744iMLFHxZrWgRqshhcI3zt-eBr0X62Ms"
+  "https://cksjplohiwqcxyfdovqp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrc2pwbG9oaXdxY3h5ZmRvdnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3OTY3MjEsImV4cCI6MjAyMjM3MjcyMX0.LSG3KbrV7e3EjLOD80FBnvXde7leVHz33hQCpZU4XIk"
 );
 export default supabase;
